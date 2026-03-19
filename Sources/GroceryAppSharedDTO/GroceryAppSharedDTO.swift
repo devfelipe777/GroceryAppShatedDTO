cdeb069752c96b1cecbba58015bd194f26afab2a
Luis Felipe Silva <eng.fsv@gmail.com>
2026-03-18 22:29:24 -0500

    Register and login response DTOs

--- Sources/GroceryAppSharedDTO/GroceryAppSharedDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryAppSharedDTO.swift
@@ -1,2 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct GroceryAppSharedDTO {
+    public private(set) var text = "Hello world"
+    
+    public init() {
+    }
+}
---
+++ Sources/GroceryAppSharedDTO/RegisterResponseDTO.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Felipe on 18/03/26.
+//
+
+import Foundation
+
+public struct RegisterResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    
+    public init(error: Bool, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
+}
